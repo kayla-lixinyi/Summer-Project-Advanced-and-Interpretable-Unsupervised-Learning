47d6113dacaa188c54880c48ec79b53c896d35a2
Daniel Tan <u7132133@anu.edu.au>
2022-01-04 11:32:40 +1100

    added documentation

--- ILS_Evaluation.py
+++ ILS_Evaluation.py
@@ -5,7 +5,70 @@ import pandas as pd
 
 class ILS_Evaluation():
     
-    def __init__(self, data_set, min_cluster_size = None, significance = 3.4):
+     """ Iterative Label Spreading Cluster Evaluation
+
+    Parameters
+    ----------
+    
+    data_set : numpy array or pandas DataFrame, 
+        The last column must be the cluster labels of the point
+
+    min_cluster_size : int, default=a third of the smallest cluster given, greater then or equal to 10
+        The minimum number of data points to be considered as a cluster.
+        
+    significance : float, default=2.56, 
+        the number of standard deviations a point must exceed the mean on 
+        either side to be considered a segmentation point
+
+    metric : String, default='euclidean'
+        The valid metric for pairwise_distance.
+        Must be a metric listed in pairwise.PAIRWISE_DISTANCE_FUNCTIONS or
+        an option allowed by scipy.spatial.distance.pdist
+
+    Attributes
+    ----------
+        
+    clusters : List of numpy arrays, 
+        each element in the list is a 2D array of points
+
+    Examples
+    --------
+    #generate clusters
+    a = np.random.uniform(0, 1, (200, 2))
+    b = np.random.uniform(2, 3, (200, 2))
+    c = np.random.uniform(4, 5, (200, 2))
+    
+    # add labels
+    a = np.concatenate((a, np.ones((200, 1))), axis = 1)
+    b = np.concatenate((b, 2 * np.ones((200, 1))), axis = 1)
+    c = np.concatenate((c, 3 * np.ones((200, 1))), axis = 1)
+    
+    data_set = np.concatenate((a, b, c), axis = 0)
+    
+    ils_eval = ILS_Evaluation(data_set, min_cluster_size = 100)
+    ils_eval.plot_cluster_rmin()
+    ils_eval.find_within_clusters()
+    
+    # now try with incorrect clustering
+    # generate clusters
+    a = np.random.uniform(0, 1, (200, 2))
+    b = np.random.uniform(2, 3, (200, 2))
+    c = np.random.uniform(4, 5, (200, 2))
+    
+    # add labels
+    a = np.concatenate((a, np.ones((200, 1))), axis = 1)
+    b = np.concatenate((b, np.ones((200, 1))), axis = 1)
+    c = np.concatenate((c, 2 * np.ones((200, 1))), axis = 1)
+    
+    data_set = np.concatenate((a, b, c), axis = 0)
+    
+    ils_eval = ILS_Evaluation(data_set, min_cluster_size = 100)
+    ils_eval.plot_cluster_rmin()
+    ils_eval.find_within_clusters()
+
+    """
+    
+    def __init__(self, data_set, min_cluster_size=None, significance=2.56, metric='euclidean'):
         
         if isinstance(data_set, pd.DataFrame):
             name = data_set.columns[-1]
@@ -28,6 +91,7 @@ class ILS_Evaluation():
         else:
             raise Exception("Invalid Data type for significance")
             
+        self.metric = metric
         self.labels = labels
         self.clusters = clusters
         self.data_set = np.array(data_set)
@@ -39,6 +103,8 @@ class ILS_Evaluation():
             self.min_cluster_size = min_cluster_size
         
     def plot_cluster_rmin(self):
+
+        #plot the rainbow coloured rmin plot for each given cluster
         
         for i in range(len(self.clusters)):
             if self.clusters[i].shape[0] < self.min_cluster_size:
@@ -46,7 +112,7 @@ class ILS_Evaluation():
                 continue
             else:
                 print("Class {}".format(self.labels[i]))
-            tempILS = ILS(min_cluster_size = self.min_cluster_size)
+            tempILS = ILS(min_cluster_size = self.min_cluster_size, metric = self.metric)
                 
             tempILS.data_set = np.concatenate((self.clusters[i], np.zeros((self.clusters[i].shape[0],1))), axis = 1)
             
@@ -61,6 +127,8 @@ class ILS_Evaluation():
             
     def find_within_clusters(self, plot_labels = False):
         
+        # find peaks within the given clusters
+        
         n_clusters = []
         
         for i in range(len(self.clusters)):
@@ -70,9 +138,9 @@ class ILS_Evaluation():
             else:
                 print("Class {}:".format(self.labels[i]))
             if self.variable_sig:
-                tempILS = ILS(min_cluster_size = self.min_cluster_size, significance = self.significance[i])
+                tempILS = ILS(min_cluster_size = self.min_cluster_size, significance = self.significance[i], metric = self.metric)
             else:
-                tempILS = ILS(min_cluster_size = self.min_cluster_size, significance = self.significance)
+                tempILS = ILS(min_cluster_size = self.min_cluster_size, significance = self.significance, metric = self.metric)
                 
             tempILS.data_set = np.concatenate((self.clusters[i], np.zeros((self.clusters[i].shape[0],1))), axis = 1)
             
